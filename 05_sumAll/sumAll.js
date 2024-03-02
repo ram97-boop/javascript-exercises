86046780097163c057cf412f3f0697b6faf2ae05
Ram Ali <ram.lugtu97@gmail.com>
2024-03-02 13:22:50 +0100

    solve exercise 5 one way

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function (n1, n2) {
+    let sum = 0; // initialize as a number
+    let smaller;
+    let bigger;
 
+    if (
+        n1 < 0 ||
+        n2 < 0 ||
+        (typeof n1) != "number" ||
+        (typeof n2) != "number"
+    ) {
+        return "ERROR";
+    }
+
+    if (n1 >= n2) {
+        smaller = n2;
+        bigger = n1;
+    }
+    else {
+        smaller = n1;
+        bigger = n2;
+    }
+
+    for (let n = smaller; n <= bigger; ++n) {
+        sum += n;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
