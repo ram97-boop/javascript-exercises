6ca3163b6dc052eaa88d3a68f100dce3e464050e
Ram Ali <ram.lugtu97@gmail.com>
2024-03-02 11:56:37 +0100

    solve exercise 3 my way

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function (str) {
+    let reversed = "";
+    for (let i = str.length - 1; i >= 0; --i) {
+        reversed += str.at(i);
+    }
 
+    return reversed;
 };
 
 // Do not edit below this line
