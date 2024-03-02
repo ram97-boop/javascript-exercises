249baa1937980ec8566f7b08606581ce5b8f6063
Ram Ali <ram.lugtu97@gmail.com>
2024-03-02 11:48:03 +0100

    solve exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
+const repeatString = function (string, n) {
+    if (n < 0) {
+        return "ERROR";
+    }
 
+    return string.repeat(n);
 };
 
 // Do not edit below this line
