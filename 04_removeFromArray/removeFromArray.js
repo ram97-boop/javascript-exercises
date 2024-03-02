406e33adb6549d9fa02322c0267b6f638d224e51
Ram Ali <ram.lugtu97@gmail.com>
2024-03-02 13:08:08 +0100

    make exercise 4 pass 4 tests so far

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...items) {
+    console.log(items);
+    let result = [...array]; // make a copy
+    let idx;
+    for (const item of items) {
+        // while there's still 'item' in the array.
+        while ((idx = result.indexOf(item)) >= 0) {
+            result.splice(idx, 1);
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
