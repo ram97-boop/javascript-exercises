a56f7158d93875c6258a03eea28ee7dbe98cfde7
Ram Ali <ram.lugtu97@gmail.com>
2024-03-07 17:35:36 +0100

    solve exercise 12 my way, needs refactoring

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    let oldestPerson;
+    let longestLife = 0;
 
+    for (let person of people) {
+        let lifeSpan;
+
+        if (!person.yearOfDeath) {
+            const todaysYear = new Date().getFullYear();
+            lifeSpan = todaysYear - person.yearOfBirth;
+        }
+        else {
+            lifeSpan = person.yearOfDeath - person.yearOfBirth;
+        }
+
+        if (lifeSpan > longestLife) {
+            oldestPerson = person;
+            longestLife = lifeSpan;
+        }
+    }
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
