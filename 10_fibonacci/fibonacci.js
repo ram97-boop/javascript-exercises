19468a0071b4dfe17e404e339372a3960e3fcc7b
Ram Ali <ram.lugtu97@gmail.com>
2024-03-07 16:49:33 +0100

    solve exercise 10 my way

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    num = parseInt(n);
+    if (num < 0) {
+        return "OOPS";
+    }
+    if (num === 0) {
+        return 0;
+    }
+    if (num === 1) {
+        return 1;
+    }
 
+    let f0 = 0;
+    let f1 = 1;
+    let f2 = 0;
+
+    for (let i = 1; i < num; ++i) {
+        f2 = f0 + f1;
+        f0 = f1;
+        f1 = f2;
+    }
+
+    return f2;
 };
 
 // Do not edit below this line
