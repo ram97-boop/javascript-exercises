3e6d1e397795d771560c20ef1b49b7d8e37ab7b9
Ram Ali <ram.lugtu97@gmail.com>
2024-03-07 15:41:25 +0100

    solve exercise 9 my way, with regex

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const noSpecialCharArray = str
+        .split("")
+        .filter((c) => c.match(/[0-9a-z]/i))
+        .map((c) => c.toLowerCase());
 
+    const reversed = [...noSpecialCharArray] // make a copy
+        .reverse();
+
+    return reversed.join("") === noSpecialCharArray.join("");
 };
 
 // Do not edit below this line
