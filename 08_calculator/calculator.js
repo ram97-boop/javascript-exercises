815c229600209aef33f84c008155baa4097f1044
Ram Ali <ram.lugtu97@gmail.com>
2024-03-07 14:42:55 +0100

    solve exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function (n1, n2) {
+  return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function (n1, n2) {
+  return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  return numbers.reduce(
+    (total, currentNum) => total + currentNum,
+    0
+  );
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  return numbers.reduce(
+    (product, currentItem) => product * currentItem
+  )
 };
 
-const power = function() {
-	
+const power = function (num, exponent) {
+  return num ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 0) {
+    return 1;
+  }
+
+  let result = 1;
+  for (let i = 1; i <= num; ++i) {
+    result *= i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
